ac5f71a9532b42d873e7c7f4625b95b4e725c496
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-04-02 14:54:18 +0100

    Forgot annotation

--- src/main/java/com/nukkitx/plugin/dmc/DisableMovementCheck.java
+++ src/main/java/com/nukkitx/plugin/dmc/DisableMovementCheck.java
@@ -1,5 +1,7 @@
 package com.nukkitx.plugin.dmc;
 
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerPreLoginEvent;
 import cn.nukkit.plugin.PluginBase;
@@ -10,6 +12,7 @@ public class DisableMovementCheck extends PluginBase implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onLogin(PlayerPreLoginEvent event) {
         event.getPlayer().setCheckMovement(false);
     }
